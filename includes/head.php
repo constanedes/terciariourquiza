d1e6799b0df1e8d50138247fe47a7f86bd32588a
Constantino <constanedes2001@gmail.com>
2022-01-13 04:19:48 -0300

    añadido bootstrap e includes

---
+++ includes/head.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- ICON  -->
+    <link rel="shortcut icon" href="../img/favicon.ico" type="image/x-icon">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.6.1/font/bootstrap-icons.css">
+
+    <!-- STYLE CSS -->
+    <link rel="stylesheet" href="../css/style.css">
+    
+    <!--  NORMALIZE CSS  -->
+    <link rel="stylesheet" href="../css/normalize.min.css"> 
+
+    <!-- BOOTSTRAP 5.1 CSS -->
+    <link rel="stylesheet" href="../css/bootstrap-5.1.0.min.css">
+
+    <title>Escuela Urquiza</title>
+    
+</head>
+<body>
+
---
+++ index.php
@@ -0,0 +1,7 @@
+
+<?php include('includes/head.php') ?>
+
+<?php include('includes/nav.php') ?>
+
+
+<?php include('includes/footer.php') ?>
