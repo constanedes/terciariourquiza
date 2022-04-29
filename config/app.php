ff87b8d05235fc5c3809994076c78befc03197b3
Matias Ramirez <matiasgr3009@gmail.com>
2022-04-29 07:24:30 -0300

    added datatables

--- config/app.php
+++ config/app.php
@@ -176,7 +176,7 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-
+        Yajra\Datatables\DatatablesServiceProvider::class,
     ],
 
     /*
@@ -192,6 +192,7 @@ return [
 
     'aliases' => Facade::defaultAliases()->merge([
         // ...
+        'Datatables' => 'Yajra\Datatables\Facades\Datatables',
     ])->toArray(),
 
 ];
