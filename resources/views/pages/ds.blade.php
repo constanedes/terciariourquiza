0e2cd4962c93853f021703056135fa5a83022c41
Constantino Edes <constantino.edes@gmail.com>
2022-09-10 16:57:45 -0300

    refactor(pages): add pages format

--- resources/views/pages/ds.blade.php
+++ resources/views/pages/ds.blade.php
@@ -1,47 +1,47 @@
 @extends('layouts.main')
 @section('content')
-    <div class="container">
+<div class="container">
     <section>
         <p>
-        Técnico Superior en Desarrollo de Software participa en proyectos de desarrollo de software desempeñando roles que
-        tienen por objeto producir artefactos de software (programas, módulos, objetos).
-        Estos artefactos suelen integrarse en aplicaciones o subsistemas que interactúan entre sí, con otras aplicaciones
-        ya
-        existentes desarrolladas con la misma o distinta tecnología, con el sistema operativo
-        del computador u otro software de base (motor de base de datos, navegador, monitor de comunicaciones) configurando
-        distintas capas de software que pueden estar distribuidas en diversas máquinas situadas
-        en la misma o distintas ubicaciones. </p>
+            Técnico Superior en Desarrollo de Software participa en proyectos de desarrollo de software desempeñando roles que
+            tienen por objeto producir artefactos de software (programas, módulos, objetos).
+            Estos artefactos suelen integrarse en aplicaciones o subsistemas que interactúan entre sí, con otras aplicaciones
+            ya
+            existentes desarrolladas con la misma o distinta tecnología, con el sistema operativo
+            del computador u otro software de base (motor de base de datos, navegador, monitor de comunicaciones) configurando
+            distintas capas de software que pueden estar distribuidas en diversas máquinas situadas
+            en la misma o distintas ubicaciones. </p>
 
         <ul>Las funciones del Técnico Superior en Desarrollo de Software son:
-        <li>Modelizar artefactos de software a partir de especificaciones, refinándolas en caso necesario,
-            para determinar el diseño detallado y las características de una solución que las satisfaga en
-            el contexto de la arquitectura del sistema de software del cual van a formar parte.</li>
-        <li>Construir los artefactos de software que implementen el diseño realizado, aplicando patrones o
-            reutilizando código en la medida en que resulte posible. Al hacer esto, y en función de lo acordado
-            para el proyecto, optimizará el desempeño de lo que construya aplicando buenas prácticas de programación
-            y documentación.</li>
-        <li>Verificar los artefactos de software construidos considerando las necesidades de cobertura de la prueba.
-            Para ello diseña los casos considerando el entorno de pruebas y ejecuta pruebas unitarias, así como registra
-            los datos y resultados. De ser necesario, realiza acciones correctivas sobre el código hasta asegurarse de que
-            cumpla con las especificaciones recibidas.</li>
-        <li>Revisar el código de artefactos de software para resolver defectos o mejorarlo. Este código puede ser propio o
-            ajeno. Esta actividad comprende revisiones cruzadas con otros integrantes del proyecto para asegurar la calidad
-            del producto. Algunas asignaciones requieren una revisión de código ya existente para poder ampliar
-            funcionalidades
-            o refactorizarlo.</li>
-        <li>Documentar sus actividades y los resultados obtenidos aportando elementos para asegurar la calidad de los
-            proyectos de acuerdo con normas y estándares establecidos.</li>
-        <li>Gestionar sus propias actividades dentro del equipo de trabajo del proyecto. Ello comprende la planificación
-            (organización y control) de las tareas a realizar, el oportuno reporte de avances y dificultades y el registro
-            y reflexión sobre lo realizado para capitalizar experiencias y estimar métricas aplicables a su actividad.</li>
-        <li>Interactuar con los diferentes roles ocupacionales y áreas organizacionales, mediante un trabajo en equipo de
-            carácter cooperativo, con capacidad para negociar, argumentar y articular propuestas, necesidades y
-            expectativas.
-        </li>
-        <li>
-            Generar propuestas innovadoras y/o emprendimientos productivos propios del ámbito del desarrollo de software.
-        </li>
+            <li>Modelizar artefactos de software a partir de especificaciones, refinándolas en caso necesario,
+                para determinar el diseño detallado y las características de una solución que las satisfaga en
+                el contexto de la arquitectura del sistema de software del cual van a formar parte.</li>
+            <li>Construir los artefactos de software que implementen el diseño realizado, aplicando patrones o
+                reutilizando código en la medida en que resulte posible. Al hacer esto, y en función de lo acordado
+                para el proyecto, optimizará el desempeño de lo que construya aplicando buenas prácticas de programación
+                y documentación.</li>
+            <li>Verificar los artefactos de software construidos considerando las necesidades de cobertura de la prueba.
+                Para ello diseña los casos considerando el entorno de pruebas y ejecuta pruebas unitarias, así como registra
+                los datos y resultados. De ser necesario, realiza acciones correctivas sobre el código hasta asegurarse de que
+                cumpla con las especificaciones recibidas.</li>
+            <li>Revisar el código de artefactos de software para resolver defectos o mejorarlo. Este código puede ser propio o
+                ajeno. Esta actividad comprende revisiones cruzadas con otros integrantes del proyecto para asegurar la calidad
+                del producto. Algunas asignaciones requieren una revisión de código ya existente para poder ampliar
+                funcionalidades
+                o refactorizarlo.</li>
+            <li>Documentar sus actividades y los resultados obtenidos aportando elementos para asegurar la calidad de los
+                proyectos de acuerdo con normas y estándares establecidos.</li>
+            <li>Gestionar sus propias actividades dentro del equipo de trabajo del proyecto. Ello comprende la planificación
+                (organización y control) de las tareas a realizar, el oportuno reporte de avances y dificultades y el registro
+                y reflexión sobre lo realizado para capitalizar experiencias y estimar métricas aplicables a su actividad.</li>
+            <li>Interactuar con los diferentes roles ocupacionales y áreas organizacionales, mediante un trabajo en equipo de
+                carácter cooperativo, con capacidad para negociar, argumentar y articular propuestas, necesidades y
+                expectativas.
+            </li>
+            <li>
+                Generar propuestas innovadoras y/o emprendimientos productivos propios del ámbito del desarrollo de software.
+            </li>
         </ul>
     </section>
-    </div>
+</div>
 @stop
